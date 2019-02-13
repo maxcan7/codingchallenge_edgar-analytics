320559fde46e59d0fb6b2bb3c8d006428f848a43
maxcan7 <maxcan7@gmail.com>
2019-02-13 16:33:30 -0500

    partially works (doesn't account for inactives yet but otherwise writes)

--- src/sessionization.py
+++ src/sessionization.py
@@ -1,6 +1,7 @@
 def sessionization(*args):
     import csv
     import numpy as np
+    from numpy import genfromtxt
     import datetime as dt
 
     # Separate input and output paths and variables into separate lists
@@ -29,7 +30,8 @@ def sessionization(*args):
             else:
                 session = np.vstack((session,np.array([user[j][0],user[j][1],user[j][2],'','',int(1),int(1)], dtype=object)))
 
-    # Evaluate sessions by looping through ip, subset to datetime relative to inactivity threshold, and calculate end datetime, duration, and request count 
+    # Evaluate sessions by looping through ip, subset to datetime relative to inactivity threshold, and calculate 
+    # end datetime, duration, and request count 
     for i in ip:
         # Find users with only one request
         if len(session[session[:,0] == i]) == 1:
@@ -39,9 +41,9 @@ def sessionization(*args):
         else:
             # Subset by IPs
             user = session[session[:,0] == i]
-            # Loop through number of instances of an IP
+            # Loop through number of requests of an IP
             for u in range(len(user)):
-                # Concatenate date and time for each instance and stack them into an array for all instances
+                # Concatenate date and time for each requests and stack them into an array for all requests
                 if 'user_datetimes' not in locals():
                     user_datetimes = np.hstack([user[u,1],user[u,2]])
                 else:
@@ -63,8 +65,25 @@ def sessionization(*args):
             # Calculate sessions, requests, and durations for each ip
             for dtidx in range(len(user_datetimes_obj)):
                 if dtidx != len(user_datetimes_obj):
-                    tdelt = int((user_datetimes_obj[dtidx][0] - user_datetimes_obj[dtidx+1][0]).seconds)
-                    tdelt > inactive
+                    # For each datetime for a user, take difference between next datetime and current. If delta > inactive, 
+                    # treat as a new session. Otherwise, add to current session request number and duration
+                    tdelt = int(user_datetimes_obj[dtidx+1][0].second) - int(user_datetimes_obj[dtidx][0].second)
+                    # Take first request of an ip, sub this into sessions after calculating
+                    req = np.take(session[session[:,0] == i],[0,1,2,3,4,5,6]) 
+                    if tdelt <= inactive:
+                        # Replace end date with that of subsequent request (will end on the last request for that session)
+                        req[3] = user[dtidx+1][1]
+                        # Replace end time with that of subsequent request (will end on the last request for that session)
+                        req[4] = user[dtidx+1][2] 
+                        # Add duration subsequent request to session duration
+                        req[5] = req[5] + int((user_datetimes_obj[dtidx+1][0].second))
+                        # Add to count of requests for session
+                        req[6] = req[6] + 1
+                        # Write to outputfile (sesspath)
+                        with open(sesspath, 'w', encoding='utf8') as outputfile:
+                             outputfile.write(','.join(req.astype(str)) + '\n')
+
+# Need to either fit req into sessions or write it to output directly and account for when tdelt > inactive, otherwise what I have so far works.                        
 
 directory = 'C:/Users/maxca/Documents/GitHub/codingchallenge_edgar-analytics'
 sessionization(directory+'/input/log.csv', directory+'/input/inactivity_period.txt', directory+'/output/sessionization.txt')
